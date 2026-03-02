500bf8fa48d16c8a5f7920d04c59c85e841840b1
dddddlllll-myz <myz800209@outlook.com>
2026-03-02 20:41:53 +0800

    优化数据库连接池类，添加登录校验流程注释，修复头文件路径大小写问题

--- CGI_MySql/Sql_Conn_Pool.h
+++ CGI_MySql/Sql_Conn_Pool.h
@@ -10,12 +10,61 @@
 #include <string>
 
 #include "../Lock/Lock.h"
-#include "../log/log.h"
+#include "../Log/Log.h"
 
 using namespace std;
 
 class Conn_Pool {
-    
-}
+public:
+	MYSQL *GetConn();				 //获取数据库连接
+	bool ReleaseConn(MYSQL *conn);   //释放连接
+	int GetFreeConn();				 //获取连接
+	void DestroyPool();				 //销毁所有连接
+
+	static Conn_Pool *GetInstance(); //单例模式
+
+	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log); //初始化数据库连接池
+    /*
+    登录校验流程
+    提取数据：从 HTTP 请求体中解析出 user=xxx & password=yyy。
+
+    获取连接：从连接池中 getConn()。
+
+    查询数据库：执行 SELECT password FROM user WHERE username = 'xxx'。
+
+    比对结果：对比数据库中的密文与用户输入的密码。
+
+    归还连接：无论成功失败，务必 releaseConn()。
+    */
+
+private:
+    Conn_Pool();
+    ~Conn_Pool();
+
+    int m_MaxConn;  //最大连接数
+	int m_CurConn;  //当前已使用的连接数
+	int m_FreeConn; //当前空闲的连接数
+	Lock lock;
+	list<MYSQL *> connList; //连接池
+	Semaphore reserve;
+
+public:
+	string m_url;			 //主机地址
+	string m_Port;		     //数据库端口号
+	string m_User;		    //登陆数据库用户名
+	string m_PassWord;	    //登陆数据库密码
+	string m_DatabaseName;  //使用数据库名
+	int m_close_log;	    //日志开关
+};
+
+class ConnectionRAII {
+public:
+	ConnectionRAII(MYSQL **con, Conn_Pool *pool);
+	~ConnectionRAII();
+	
+private:
+	MYSQL *connRAII;
+	Conn_Pool *poolRAII;
+};
 
 #endif
