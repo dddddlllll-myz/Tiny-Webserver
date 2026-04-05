9367231cc66ec9a9b4a6e0dbf576b0d2c605d0a8
MYZ <myz800209@outlook.com>
2026-04-06 04:46:13 +0800

    添加主程序入口，初始化Webserver和数据库连接池，完善命令行参数解析功能

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "Config.h"
+
+int main(int argc, char* argv[]) {
+    //需要修改的数据库信息,登录名,密码,库名
+    string user = "root";
+    string passwd = "root";
+    string databasename = "qgydb";
+
+    //命令行解析
+    Config config;
+    config.parse_arg(argc, argv);
+
+    Webserver server;
+    server.init(config.PORT, user, passwd, databasename, config.LOGWrite, config.OPT_LINGER,
+                config.TRIGMode, config.sql_num, config.thread_num, config.close_log, config.actor_model);
+
+    server.log_write(); // 日志系统初始化
+
+    server.sql_pool(); // 数据库连接池初始化
+
+    server.thread_pool(); // 线程池初始化
+
+    server.trig_mode(); // 触发模式设置
+
+    server.eventListen(); // 监听事件
+
+    server.eventLoop(); // 事件循环
+
+    return 0;
+}
